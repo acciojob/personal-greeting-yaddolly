c2abf8ed96802b846f1bf4d111b94b53e1adfce9
AAKASH _YADAV <yaddolly>
2024-09-21 20:59:34 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from 'react'
 
 const App = () => {
+  const [text, setText] = useState()
+  console.log(text)
   return (
     <div>
-        {/* Do not remove the main div */}
+      
+
+      <label htmlFor='ip'>Enter your name:</label><br/>
+      <input id='ip' type='text' onChange={(e) => {
+        setText(e.target.value)
+      }} />
+
+      {/* {text && <p>Hello {text}!</p>} */}
+
+      <p>{text && `Hello ${text}!`}</p>
+
     </div>
   )
 }
